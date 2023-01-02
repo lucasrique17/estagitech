39c151fcf0d82cf97bf548d377349ae634872918
lucasrique17 <henrique-llucas@hotmail.com>
2023-01-02 15:33:40 -0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+import {BrowserRouter, Routes, Route,Navigate} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <h1>EstagiTech</h1>
+      
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+
+          </Routes>
+        </div>
+      </BrowserRouter>
+
     </div>
   );
 }
