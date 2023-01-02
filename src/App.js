bb8d6e28d33be328f223454d1e12c557b9c336b1
lucasrique17 <henrique-llucas@hotmail.com>
2023-01-02 19:28:07 -0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import {BrowserRouter, Routes, Route,Navigate} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
 function App() {
   return (
@@ -9,13 +9,13 @@ function App() {
       <BrowserRouter>
         <div className="container">
           <Routes>
-
+            
           </Routes>
         </div>
       </BrowserRouter>
 
     </div>
   );
-}
+};
 
 export default App;
